77e212f9fa5c8775eb7e64ecd763b9841d26e4ac
mike dupont <mike.dupont@introspector.local>
2024-02-05 11:02:11 -0500

    you should never have come here

--- llmga/diffusers/pip.sh
+++
@@ -1,3 +0,0 @@
-pip install datasets --index-url https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
-pip install . --index-url https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
-pip install albumentations --index-url https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
